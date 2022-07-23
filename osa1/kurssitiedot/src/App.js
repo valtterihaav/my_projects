d58dee4edbba4feb2cd63d7e372baa6eb32c24f0
valtterihaav <vade.haavisto@gmail.com>
2022-07-23 20:38:55 +0300

    Saatu kurssitiedot tehtävän 1.3 osuus valmiiksi

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -40,19 +40,25 @@ const Total = (props) =>
 const App = () => 
 {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
         <Header kurssi={course}/>
-        <Content osa1={part1} osa2={part2} osa3={part3} 
-        exe1={exercises1} exe2={exercises2} exe3={exercises3}/>
-        <Total ex1={exercises1} ex2={exercises2} ex3={exercises3}/>
+        <Content osa1={part1.name} osa2={part2.name} osa3={part3.name} 
+        exe1={part1.exercises} exe2={part2.exercises} exe3={part3.exercises}/>
+        <Total ex1={part1.exercises} ex2={part2.exercises} ex3={part3.exercises}/>
     </div>
   )
 }
